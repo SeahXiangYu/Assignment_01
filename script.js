e4052ca22944e8a0b78b902a39c8037f0beb10ac
SeahXiangYu <s10240016@connect.np.edu.sg>
2022-11-19 18:11:52 +0800

    Side Navigation Bar

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function myFunction(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
+}
+
+function toggleNav() {
+  var element = document.getElementById("mySidenav");
+  if (element.style.width == "97%") {
+    element.style.width = "0px";
+  } else {
+    element.style.width = "97%";
   }
+}
